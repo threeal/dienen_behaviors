b5aba493a9c64096c57959b188d53fa2ab4f9974
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-05-27 23:23:17 +0700

    feat: use argparse in the maneuver until

--- bin/maneuver_until.cpp
+++ bin/maneuver_until.cpp
@@ -18,15 +18,47 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#include <argparse/argparse.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tosshin_cpp/tosshin_cpp.hpp>
 
 #include <memory>
+#include <string>
 
 using namespace std::chrono_literals;
 
 int main(int argc, char ** argv)
 {
+  auto program = argparse::ArgumentParser("maneuver_until", "0.1.0");
+
+  program.add_argument("duration")
+  .help("Duration until finished in seconds")
+  .action([](const std::string & value) {return std::stod(value);});
+
+  program.add_argument("-f", "--forward")
+  .help("Set forward maneuver in meter per minute")
+  .action([](const std::string & value) {return std::stod(value);})
+  .default_value(0.0);
+
+  program.add_argument("-l", "--left")
+  .help("Set left maneuver in meter per minute")
+  .action([](const std::string & value) {return std::stod(value);})
+  .default_value(0.0);
+
+  program.add_argument("-y", "--yaw")
+  .help("Set yaw maneuver in radian per minute")
+  .action([](const std::string & value) {return std::stod(value);})
+  .default_value(0.0);
+
+  // Try to parse arguments
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::runtime_error & err) {
+    std::cout << err.what() << std::endl;
+    std::cout << program;
+    return 1;
+  }
+
   rclcpp::init(argc, argv);
 
   auto node = std::make_shared<rclcpp::Node>("maneuver_until");
@@ -37,11 +69,15 @@ int main(int argc, char ** argv)
 
   rclcpp::TimerBase::SharedPtr update_timer;
 
+  // Update process
   update_timer = node->create_wall_timer(
     10ms, [&]() {
       auto duration = node->now() - start_time;
-      if (duration.seconds() >= 10.0) {
+      if (duration.seconds() >= program.get<double>("duration")) {
         RCLCPP_INFO(node->get_logger(), "Finished!");
+
+        // Set maneuver into stop
+        maneuver_consumer->set_maneuver(tosshin_cpp::Maneuver());
 
         // Stop update timer
         update_timer->cancel();
@@ -66,9 +102,9 @@ int main(int argc, char ** argv)
       } else {
         auto maneuver = maneuver_consumer->get_maneuver();
 
-        maneuver.forward = 20.0;
-        maneuver.left = 0.0;
-        maneuver.yaw = 0.0;
+        maneuver.forward = program.get<double>("--forward");
+        maneuver.left = program.get<double>("--left");
+        maneuver.yaw = program.get<double>("--yaw");
 
         maneuver_consumer->set_maneuver(maneuver);
       }
