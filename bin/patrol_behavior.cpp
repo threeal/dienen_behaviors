20347acec05bddf9c5c7124d228fc0707b755242
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-06-20 12:55:39 +0700

    feat: use std topics in patrol behavior and remove class

--- bin/patrol_behavior.cpp
+++ bin/patrol_behavior.cpp
@@ -19,16 +19,22 @@
 // THE SOFTWARE.
 
 #include <argparse/argparse.hpp>
-#include <dienen_behaviors/dienen_behaviors.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <keisan/keisan.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <tf2/utils.h>
 
+#include <algorithm>
+#include <list>
 #include <memory>
 #include <string>
 #include <vector>
 
 using namespace std::chrono_literals;
 
-std::shared_ptr<dienen_behaviors::PatrolBehavior> patrol_behavior;
+using geometry_msgs::msg::Twist;
+using nav_msgs::msg::Odometry;
 
 int main(int argc, char ** argv)
 {
@@ -43,23 +49,28 @@ int main(int argc, char ** argv)
   .help("list of target points in `x1 y1 x2 y2 ...` format")
   .remaining();
 
+  std::list<keisan::Point2> target_points;
+  bool repeat;
+
   try {
     program.parse_args(argc, argv);
-  } catch (const std::runtime_error & err) {
-    std::cout << err.what() << std::endl;
+
+    // Parse target points
+    auto points = program.get<std::vector<std::string>>("points");
+    if (points.size() < 2) {
+      throw std::runtime_error("points must contain atleast one x and one y");
+    } else if (points.size() % 2 != 0) {
+      throw std::runtime_error("missing y for the last target point");
+    } else {
+      for (size_t i = 1; i < points.size(); i += 2) {
+        target_points.push_back({stod(points[i - 1]), stod(points[i])});
+      }
+    }
+
+    repeat = program.get<bool>("--repeat");
+  } catch (const std::exception & e) {
+    std::cout << e.what() << std::endl;
     std::cout << program;
-    return 1;
-  }
-
-  auto points = program.get<std::vector<std::string>>("points");
-
-  if (points.size() < 2) {
-    std::cout << "points must contain atleast one x and one y" << std::endl;
-    return 1;
-  }
-
-  if (points.size() % 2 != 0) {
-    std::cout << "missing y for the last target point" << std::endl;
     return 1;
   }
 
@@ -67,34 +78,56 @@ int main(int argc, char ** argv)
 
   auto node = std::make_shared<rclcpp::Node>("patrol_behavior");
 
-  patrol_behavior = std::make_shared<dienen_behaviors::PatrolBehavior>(node);
+  keisan::Point2 current_position;
+  double current_orientation;
+  auto odometry_subscription = node->create_subscription<Odometry>(
+    "/odom", 10,
+    [&](const Odometry::SharedPtr msg) {
+      current_position = keisan::Point2(msg->pose.pose.position.x, msg->pose.pose.position.y);
 
-  for (size_t i = 1; i < points.size(); i += 2) {
-    patrol_behavior->add_point(stof(points[i - 1]), stof(points[i]));
-  }
+      double yaw, pitch, roll;
+      tf2::getEulerYPR(msg->pose.pose.orientation, yaw, pitch, roll);
+      current_orientation = tf2Degrees(yaw);
+    });
 
-  if (program.get<bool>("--repeat")) {
-    patrol_behavior->enable_repeat(true);
-  }
+  auto twist_publisher = node->create_publisher<Twist>("/cmd_vel", 10);
 
-  // Stop maneuver on keyboard interrupt
-  signal(
-    SIGINT, [](int /*code*/) {
-      // Create a timeout handler
-      auto timer = patrol_behavior->get_node()->create_wall_timer(
-        3s, []() {
-          RCLCPP_ERROR(patrol_behavior->get_node()->get_logger(), "Failed to stop the maneuver!");
-          rclcpp::shutdown();
-        });
+  auto target_point = target_points.begin();
+  auto update_timer = node->create_wall_timer(
+    10ms, [&]() {
+      Twist twist;
 
-      RCLCPP_INFO(patrol_behavior->get_node()->get_logger(), "Requesting to stop the maneuver...");
-      patrol_behavior->configure_maneuver(
-        tosshin_cpp::Maneuver(), [timer](const tosshin_cpp::Maneuver & /*maneuver*/) {
-          RCLCPP_INFO(patrol_behavior->get_node()->get_logger(), "Maneuver stopped!");
+      // Shift target point if near
+      auto distance = keisan::Point2::distance_between(current_position, *target_point);
+      if (distance <= 0.3) {
+        ++target_point;
+        if (target_point == target_points.end()) {
+          if (repeat) {
+            target_point = target_points.begin();
+          } else {
+            RCLCPP_INFO(node->get_logger(), "Finished!");
 
-          timer->cancel();
-          rclcpp::shutdown();
-        });
+            // Set movement into stop
+            twist_publisher->publish(Twist());
+
+            rclcpp::shutdown();
+          }
+        }
+      }
+
+      // Calculate a new target angular movement
+      {
+        double direction = (*target_point - current_position).direction();
+        double delta = keisan::delta_deg(current_orientation, keisan::rad_to_deg(direction));
+
+        twist.angular.z = keisan::clamp_number(keisan::deg_to_rad(delta) * 3, -2.0, 2.0);
+      }
+
+      // Calculate a new target linear movement
+      double forward = keisan::map_number(std::abs(twist.angular.z), 0.0, 2.0 / 3, 1.0, 0.0);
+      twist.linear.x = std::max(forward, 0.0);
+
+      twist_publisher->publish(twist);
     });
 
   rclcpp::spin(node);
